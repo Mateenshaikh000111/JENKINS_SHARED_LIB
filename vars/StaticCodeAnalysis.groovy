8c9c613869fd17d53277b8103a571acec487ba2d
mateenshaik <mateenshaikh046@gmail.com>
2023-07-19 12:05:17 +0530

    static code analysis

--- vars/StaticCodeAnalysis.groovy
+++ vars/StaticCodeAnalysis.groovy
@@ -1,3 +1,6 @@
-def call(){
-    sh 'mvn clean package sonar:sonar'
+def call(credentialsId){
+
+    withSonarQubeEnv(credentialsId: 'SonarQube_API') {
+      sh 'mvn clean package sonar:sonar'
+    }
 }
