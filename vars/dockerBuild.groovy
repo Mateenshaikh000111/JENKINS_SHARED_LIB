7c3a9cbac5cf4ee01bef35bc2b388d6d7ba5d701
mateenshaik <mateenshaikh046@gmail.com>
2023-07-22 09:36:02 +0530

    add quality gate  analysis

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String project, String ImageTage, String hubUser){
     
-    sh """"
+    sh """
         docker image build -t ${hubUser}/${project} .
         docker image tag  ${hubUser}/${project} ${hubUser}/${project}:${ImageTage}   
         docker image tag  ${hubUser}/${project} ${hubUser}/${project}:latest   
