6d1b19c1b200f737872570f5b85bf3576cc433cb
mateenshaik <mateenshaikh046@gmail.com>
2023-08-01 08:29:43 +0530

    scaning stage added

--- vars/dockerimageCleanup.groovy
+++ vars/dockerimageCleanup.groovy
@@ -1,7 +1,22 @@
-def call(String project, String ImageTage, String hubUser){
+// def call(String project, String ImageTage, String hubUser){
     
+//     sh """
+//             docker rmi ${hubUser}/${project}:${ImageTage}   
+//              docker rmi  ${hubUser}/${project} ${hubUser}/${project}:latest   
+//     """
+// }
+
+
+
+
+def call(String aws_account_id, String region, String ecr_repoName){
+
     sh """
-            docker rmi ${hubUser}/${project}:${ImageTage}   
-             docker rmi  ${hubUser}/${project} ${hubUser}/${project}:latest   
+               
+         
+                docker rmi ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+
     """
+
+
 }
