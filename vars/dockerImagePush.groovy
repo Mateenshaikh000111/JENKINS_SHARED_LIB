0d1a7b85c0a1ece6236baa4e8a192e0ca1b0a40e
mateenshaik <mateenshaikh046@gmail.com>
2023-08-01 08:16:24 +0530

    scaning stage added

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,14 +1,28 @@
-def call(String project, String ImageTage, String hubUser){
+// def call(String project, String ImageTage, String hubUser){
 
-withCredentials([usernamePassword(
-    credentialsId: 'Docker',
-     passwordVariable: 'PASS', 
-     usernameVariable: 'USER'
-     )]) {
+// withCredentials([usernamePassword(
+//     credentialsId: 'Docker',
+//      passwordVariable: 'PASS', 
+//      usernameVariable: 'USER'
+//      )]) {
     
-       sh "docker login -u '$USER' -p '$PASS'"
-}    
- sh "docker image push ${hubUser}/${project}:${ImageTage}"
- sh "docker image push ${hubUser}/${project}:latest"
+//        sh "docker login -u '$USER' -p '$PASS'"
+// }    
+//  sh "docker image push ${hubUser}/${project}:${ImageTage}"
+//  sh "docker image push ${hubUser}/${project}:latest"
+
+// }
+
+
+
+def call(String aws_account_id, String region, String ecr_repoName){
+
+    sh """
+               
+               aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+               docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+
+    """
+
 
 }
